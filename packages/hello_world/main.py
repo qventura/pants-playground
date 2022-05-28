03f12ed2b240c390138caa634b5f71f8ee564c68
Quentin <quentin@guiker.com>
2022-05-28 18:25:09 -0400

    Cleanup

--- packages/hello_world/main.py
+++ packages/hello_world/main.py
@@ -7,7 +7,5 @@ app = Flask(__name__)
 def main():    
     return "Success"
 
-print("__name__", __name__)
-
 if __name__ == "hello_world.main":
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
