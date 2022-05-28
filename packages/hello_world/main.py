9eea64b01c6cd86077039b0d34d9f89df78dc63a
Quentin <quentin@guiker.com>
2022-05-28 17:56:14 -0400

    Fix docker hello world

--- packages/hello_world/main.py
+++ packages/hello_world/main.py
@@ -1,13 +1,17 @@
 import os 
 from flask import Flask
 
+print("OK1")
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():    
-    print("SUCCESS")
+    print("SUCCESSSSSS")
 
     return "Success"
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+print("__name__", __name__)
+
+if __name__ == "hello_world.main":
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
